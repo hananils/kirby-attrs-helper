e8ab37aa531eb897c7e76a7915c3703d5713c16d
Nils Hörrmann <nils.hoerrmann@hananils.de>
2024-10-01 16:29:09 +0200

    Improve implementation and add docs

--- index.php
+++ index.php
@@ -1,33 +1,53 @@
 <?php
 
-function attrs($attrs = null)
+use Kirby\Cms\App as Kirby;
+use Kirby\Cms\Html;
+
+/**
+ * Helper to conditionally create attributes for HTML elements. It takes an
+ * associative array with the keys specifying the attribute names and the values
+ * specifying the corresponing attribute values. If the value itself is an
+ * array, the keys will be used a attribute values, joined by a separator.
+ * Before joining keys, the assigned value will be used as a condition that
+ * either has to evaluate to `true` or `false`. Keys that have their condition
+ * return `false` will be discarded on not be included in the attribute value.
+ * Empty attributes will also be discarded. If you want to include a boolean
+ * attribute, that does not have a value, set the value to `true`.
+ *
+ * @param $attrs Array of attributes.
+ * @param $separator The separator used to join attribute values.
+ */
+function attrs(array $attrs, string $separator = ' '): string
 {
-    if (!$attrs) {
+    if (empty($attrs)) {
         return '';
     }
 
-    $attrs = array_map('chainValues', $attrs);
+    foreach ($attrs as $name => $tokens) {
+        if (!is_array($tokens)) {
+            continue;
+        }
 
-    return Html::attr($attrs);
-}
+        // Remove tokens with untrue conditions
+        $tokens = array_filter($tokens);
 
-function chainValues($value)
-{
-    if (is_array($value)) {
-        $values = [];
-
-        // Get valid values
-        foreach ($value as $token => $condition) {
-            if ($condition === true) {
-                $values[] = $token;
+        /**
+         * Set valid tokens as value.
+         *
+         * @note Don't use `array_keys` because there might be plain values
+         * without condition. This loop will keep string values already present
+         * in the array values.
+         */
+        foreach ($tokens as $token => $condition) {
+            if (is_bool($condition)) {
+                $tokens[$token] = $token;
             }
         }
 
-        // Remove empty values
-        $values = array_filter($values);
-
-        return implode(' ', $values);
+        $attrs[$name] = implode($separator, $tokens);
     }
 
-    return $value;
+    return Html::attr(array_filter($attrs)) ?? '';
 }
+
+Kirby::plugin('hananils/attrs-helper', []);
