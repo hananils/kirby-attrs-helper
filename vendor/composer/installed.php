40a813f9244ef259e0d1884463841c4dbed7ac1c
Nils Hörrmann <nils.hoerrmann@hananils.de>
2026-03-12 15:54:47 +0100

    Clean-up

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,18 +1,18 @@
 <?php return array(
     'root' => array(
         'name' => 'hananils/kirby-attrs-helper',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
+        'pretty_version' => '2.0.0',
+        'version' => '2.0.0.0',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'dev' => true,
     ),
     'versions' => array(
         'hananils/kirby-attrs-helper' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
